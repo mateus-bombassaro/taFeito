44112c98bc6a60e413b2d2dfdc459fb79b4bfc6d
Mateus Bombassaro Pinho <mateusbombassaropinho@MacBook-Air-de-Mateus.local>
2023-09-16 10:13:25 -0300

    feat: add nav on task page

--- src/pages/tasks/Tasks.tsx
+++ src/pages/tasks/Tasks.tsx
@@ -1,8 +1,94 @@
+import React from "react";
+
+import {AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Tooltip, MenuItem} from '@mui/material';
+import {TaskAlt, Logout} from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
+
+
 const Tasks = () => {
 
+  const navigate = useNavigate();
+
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const logout = () => {
+    setAnchorElUser(null);
+    navigate('/');
+  }
+
   return (
-      <div>Tarefas</div>
-  )
-}
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters sx={{
+          justifyContent: 'space-between'
+        }}>
+          <Box sx={{
+            display: 'flex',
+            alignItems: 'center'
+          }}>
+            <TaskAlt sx={{ display: "flex", mr: 2 }} />
+
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: "flex",
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              TaFeito
+            </Typography>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Usuario Anonimo" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem onClick={logout}>
+                <Logout sx={{ display: "flex", mr: 2 }} />
+                <Typography textAlign="center">Sair</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default Tasks;
