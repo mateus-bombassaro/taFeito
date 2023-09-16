ddd97fbb92f5e4fddd773ea0c3ae7ee7ee6c9406
Mateus Bombassaro Pinho <mateusbombassaropinho@MacBook-Air-de-Mateus.local>
2023-09-15 23:00:09 -0300

    feat: backend integration

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from 'react';
 import { 
   Box, 
   CardHeader, 
@@ -5,13 +6,64 @@ import {
   CardContent, 
   Button, 
   TextField, 
-  Stack 
+  Stack,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  FilledInput,
+  IconButton,
+  CardActions,
+  Typography,
 } from '@mui/material';
+import { VisibilityOff, Visibility } from '@mui/icons-material';
 
-import {CustomizedCardHeader} from './styles';
+import { CustomizedCardHeader } from './styles';
 
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string|null>(null);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  }
+
+  const isDisabled = useMemo(() => !(username && password), [username, password]);
+
+  const postLogin = () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        login: username,
+        senha: password
+      })
+  };
+    setErrorMessage('');
+    fetch('http://localhost:3000/usuarios/login', requestOptions)
+      .then(async (response) => {
+        const dataResponse = await response.json()
+        return {
+          responseStatus: response.status,
+          data: dataResponse
+        }
+      })
+      .then(data => {
+        console.log('sucesso', JSON.stringify(data))
+        console.log(data)
+        if(data.responseStatus === 422 && data.data?.mensagem) {
+          setErrorMessage(data.data?.mensagem)
+        } else if(data.responseStatus === 400) {
+          setErrorMessage('Requisição inválida!')
+        } else if(data.responseStatus === 200) {
+          alert('Requisição válida!')
+        } 
+      })
+      .catch(error => setErrorMessage('Erro no servidor, tente novamente em alguns minutos!'));
+  }
+
   return (
     <Box sx={{
       width: "100%", 
@@ -27,13 +79,54 @@ const Login = () => {
         <CardHeader  titleTypographyProps={{color: 'text.primary', textAlign: 'center'}} />
         <CardContent>
           <Stack spacing={2} direction="column">
-            <TextField id="username" label="Usuário" variant="filled" />
-            <TextField type="password" id="password" label="Senha" variant="filled" />
+            <TextField id="username" label="Usuário" variant="filled" onChange={(e) => setUsername(e.target.value)} />
+            <FormControl sx={{ width: '100%' }} variant="filled">
+              <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
+              <FilledInput
+                onChange={(newValue) => {
+                  setPassword(newValue.target.value);
+                }}
+                id="filled-adornment-password"
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           </Stack>
         </CardContent>
-        <CardContent>
-          <Button fullWidth variant="contained">Login</Button>
-        </CardContent>
+        <CardActions>
+          <Box sx={{
+            display:'flex',
+            flexDirection:'row',
+            width:'100%',
+            flexWrap: 'wrap'
+          }}>
+            <Box width={'100%'} textAlign="center">
+              {errorMessage && <Typography color={'red'}>
+                {errorMessage}
+              </Typography>}
+            </Box>
+            <Box width={'100%'}>
+              <Button
+                sx={{
+                  width: '100%'
+                }}
+                onClick={() => {postLogin()}}
+                disabled={isDisabled} fullWidth variant="contained">
+                  Login
+              </Button>
+            </Box>
+          </Box>
+        </CardActions>
       </Card>
     </Box>
   )
