be0a8a71078aae53f9adf256aa3e00af97351ff3
Mateus Bombassaro Pinho <mateusbombassaropinho@MacBook-Air-de-Mateus.local>
2023-09-30 12:22:52 -0300

    feat: add task status

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -3,7 +3,7 @@ import { Tarefa } from '../../utils/model';
 
 import { Box } from "@mui/material";
 import { useState } from "react";
-
+import { format, formatDistance, formatRelative, subDays,parseISO } from 'date-fns';
 import {
   ListItem,
   ListItemButton,
@@ -17,7 +17,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import { useGlobalContext } from '../../utils/global';
 import DeleteTaskDialog from '../DeleteTaskDialog/DeleteTaskDialog';
 import { useSnackbar } from 'notistack';
-import { url_update_task } from '../../utils/api';
+import { url_update_task, url_finish_task, url_reopen_task } from "../../utils/api";
 import axios from 'axios';
 
 type TaskProps = {
@@ -33,9 +33,45 @@ const Task = (props: TaskProps) => {
   const [error, setError] = useState<null | string>(null);
 
   const [openedDialog, setOpenedDialog] = useState(false);
-  const [checked, setChecked] = useState([0]);
+  const [checked, setChecked] = useState(task.data_conclusao ? [task.id] : [0]);
   const { enqueueSnackbar } = useSnackbar();
   const labelId = `checkbox-list-label-${task.id}`;
+
+  const finishTask = async () => {
+    const taskId = task?.id ?? -1;
+    const custom_task_url = url_finish_task.replace(":id", taskId.toString());
+    try {
+      await axios.post(custom_task_url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setError(null);
+      enqueueSnackbar("Tarefa concluída!", { variant: "success" });
+      setRefectchTaskStatus(refetchTaskStatus + 1);
+    } catch (err) {
+      setError((err as Error).message);
+      enqueueSnackbar("Erro ao tentar concluir a tarefa.", { variant: "error" });
+    }
+  };
+
+  const reopenTask = async () => {
+    const taskId = task?.id ?? -1;
+    const custom_task_url = url_reopen_task.replace(":id", taskId.toString());
+    try {
+      await axios.post(custom_task_url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setError(null);
+      enqueueSnackbar("Tarefa reaberta!", { variant: "success" });
+      setRefectchTaskStatus(refetchTaskStatus + 1);
+    } catch (err) {
+      setError((err as Error).message);
+      enqueueSnackbar("Erro ao tentar reabrir a tarefa.", { variant: "error" });
+    }
+  };
 
   const handleToggle = (value: number) => () => {
     const currentIndex = checked.indexOf(value);
@@ -43,8 +79,10 @@ const Task = (props: TaskProps) => {
 
     if (currentIndex === -1) {
       newChecked.push(value);
+      finishTask();
     } else {
       newChecked.splice(currentIndex, 1);
+      reopenTask();
     }
 
     setChecked(newChecked);
@@ -67,6 +105,14 @@ const Task = (props: TaskProps) => {
       enqueueSnackbar("Erro ao deletar a tarefa.", { variant: "error" });
     }
   };
+
+  const renderFinishedText = () => {
+    if (task.data_conclusao) {
+      return format(parseISO(task.data_conclusao), "'Concluído em' dd/MM/yyyy")
+    }
+    return;
+  }
+
   return (
     <>
       <ListItem
@@ -108,7 +154,7 @@ const Task = (props: TaskProps) => {
               inputProps={{ "aria-labelledby": labelId }}
             />
           </ListItemIcon>
-          <ListItemText id={labelId} primary={task.descricao} />
+          <ListItemText id={labelId} primary={task.descricao} secondary={renderFinishedText()}/>
         </ListItemButton>
       </ListItem>
       <DeleteTaskDialog
